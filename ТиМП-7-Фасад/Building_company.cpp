52aa2fbaa4790b4cc1ac906591b1373c0a52aa90
V-Mor <vvovv266@gmail.com>
2018-05-15 22:52:31 +0700

    little correctons done

--- ТиМП-7-Фасад/Building_company.cpp
+++ ТиМП-7-Фасад/Building_company.cpp
@@ -2,20 +2,28 @@
 
 void Building_company::buildCheapHouse()
 {
+	cout << "Sponsor: ";
 	sp->giveSomeMoney();
+	cout << "Architect: ";
 	arch->makeCheapHouseProject();
+	cout << "Building factory: ";
 	bFact->produceCheapMaterials();
+	cout << "Workers: ";
 	wrks->workLite(); 
-	cout << "Your house is done!\n";
+	cout << "Your house is done!\n\n";
 }
 
 void Building_company::buildExpensiveHouse()
 {
+	cout << "Sponsor: ";
 	sp->giveLotsOfMoney();
+	cout << "Architect: ";
 	arch->makeExpensiveHouseProject();
+	cout << "Building factory: ";
 	bFact->produceExpensiveMaterials();
+	cout << "Workers: ";
 	wrks->workHard();
-	cout << "Your house is done!\n";
+	cout << "Your house is done!\n\n";
 }
 
 Building_company::Building_company()
